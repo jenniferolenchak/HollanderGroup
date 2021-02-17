2211ba828c2dd6b67d67861694b34e42d647b9f7
Burke Hill <burkehill@burkes-imac.home>
2021-02-17 16:45:25 -0500

    stuff

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from home import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='home/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='home/logout.html'), name='logout'),
+    path('register/', home_views.register, name='register'),
 ]
