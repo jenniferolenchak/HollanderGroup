ad9470a9342ee318c365750ddd1ccff56a24e5ed
Perl-R <robinaaperl@gmail.com>
2021-02-21 19:05:11 -0500

    Create New Url Automated Tests

--- Project_Directory/home/tests/test_urls.py
+++ Project_Directory/home/tests/test_urls.py
@@ -16,3 +16,12 @@ class testUrls(SimpleTestCase):
 	def test_register_url_resolves(self):
 		url = reverse('register')
 		self.assertEquals(resolve(url).func, register)
+
+
+	def test_login_url_resolves(self):
+		url = reverse('login')
+		self.assertEquals(resolve(url).func, login)
+
+	def test_logout_url_resolves(self):
+		url = reverse('logout')
+		self.assertEquals(resolve(url).func,logout)
