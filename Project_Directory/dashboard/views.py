479116ff4482c83bc7173708a258dae2697ed9f1
Perl-R <robinaaperl@gmail.com>
2021-03-04 01:18:46 -0500

    Allow for Editing Budget

--- Project_Directory/dashboard/views.py
+++ Project_Directory/dashboard/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.db import models
+from .models import *
 
 # Create your views here.
 
 @login_required(login_url="login")
 def dashboard(request):
+	if request.method == 'POST':
+		budgetInput = request.POST.get('budgetInput')
+		
+		request.user.budget.balance = budgetInput
+		request.user.budget.save()
+
+
 	return render(request, 'DashboardTemplates/dashboard.html')
+
+
 
 @login_required(login_url='login')
 def settings(request):
