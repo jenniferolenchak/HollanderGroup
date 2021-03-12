27ed5d604d6631e4e2286034831235eebf57a384
Perl-R <robinaaperl@gmail.com>
2021-03-12 16:48:32 -0500

    Create Automated Login and Registration Tests with Selenium

---
+++ Project_Directory/home/tests/test_login_functionality.py
@@ -0,0 +1,54 @@
+# test_login_functionality
+
+
+from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+import time
+from django.contrib.auth.models import User
+
+# Note Currently Using ChromeWebDriver version 89
+# ConnectionResetError may occur however they do not impact the validity of tests
+class TestLoginPage(StaticLiveServerTestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Chrome('Home/tests/chromedriver.exe')
+
+	def tearDown(self):
+		self.browser.close()
+
+
+	def test_login_with_valid_user_redirects_to_dashboard(self):
+		user = User.objects.create_user('username', 'username@gmail.com', 'password')
+		user.save()
+
+		self.browser.get(self.live_server_url + reverse('login'))
+
+		username_element = self.browser.find_element_by_id("username").send_keys('username')
+		password_element = self.browser.find_element_by_id("password").send_keys('password')
+
+		self.browser.find_element_by_xpath("//button[@type='submit']").click()
+
+		dashboard_url = self.live_server_url + reverse('dashboard')
+		
+		self.assertEquals(
+			self.browser.current_url,
+			dashboard_url
+			)
+
+	def test_login_with_invalid_user_does_not_redirect_to_dashboard(self):
+		self.browser.get(self.live_server_url + reverse('login'))
+
+		username_element = self.browser.find_element_by_id("username").send_keys('not_a_user')
+		password_element = self.browser.find_element_by_id("password").send_keys('not_a_user')
+
+		self.browser.find_element_by_xpath("//button[@type='submit']").click()
+
+		dashboard_url = self.live_server_url + reverse('login')
+		
+		self.assertEquals(
+			self.browser.current_url,
+			dashboard_url
+			)
+
+
---
+++ Project_Directory/home/tests/test_registration_functionality.py
@@ -0,0 +1,108 @@
+# test_registration_functionality.py
+
+
+from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+import time
+from django.contrib.auth.models import User
+
+# Note Currently Using ChromeWebDriver version 89
+# ConnectionResetError may occur however they do not impact the validity of tests
+class TestLoginPage(StaticLiveServerTestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Chrome('Home/tests/chromedriver.exe')
+
+	def tearDown(self):
+		self.browser.close()
+
+
+	def test_registration_with_valid_user_redirects_to_login(self):
+
+		self.browser.get(self.live_server_url + reverse('register'))
+
+		username_element = self.browser.find_element_by_id("id_username").send_keys('username')
+		first_name_element = self.browser.find_element_by_id('id_first_name').send_keys('user')
+		first_name_element = self.browser.find_element_by_id('id_last_name').send_keys('name')
+		password_element = self.browser.find_element_by_id("id_email").send_keys('user@name.com')
+
+		password_element = self.browser.find_element_by_id("id_password1").send_keys('wordpass1234!')
+		password_element = self.browser.find_element_by_id("id_password2").send_keys('wordpass1234!')
+
+
+		self.browser.find_element_by_xpath("//button[@type='submit']").click()
+
+		dashboard_url = self.live_server_url + reverse('login')
+		
+		self.assertEquals(
+			self.browser.current_url,
+			dashboard_url
+			)
+
+
+	def test_registration_with_common_password_is_not_permitted(self):
+
+		self.browser.get(self.live_server_url + reverse('register'))
+
+		username_element = self.browser.find_element_by_id("id_username").send_keys('username')
+		first_name_element = self.browser.find_element_by_id('id_first_name').send_keys('user')
+		first_name_element = self.browser.find_element_by_id('id_last_name').send_keys('name')
+		password_element = self.browser.find_element_by_id("id_email").send_keys('user@name.com')
+
+		password_element = self.browser.find_element_by_id("id_password1").send_keys('password')
+		password_element = self.browser.find_element_by_id("id_password2").send_keys('password')
+
+
+		self.browser.find_element_by_xpath("//button[@type='submit']").click()
+
+		dashboard_url = self.live_server_url + reverse('register')
+		
+		self.assertEquals(
+			self.browser.current_url,
+			dashboard_url
+			)
+
+	def test_registration_with_short_password_is_not_permitted(self):
+
+		self.browser.get(self.live_server_url + reverse('register'))
+
+		username_element = self.browser.find_element_by_id("id_username").send_keys('username')
+		first_name_element = self.browser.find_element_by_id('id_first_name').send_keys('user')
+		first_name_element = self.browser.find_element_by_id('id_last_name').send_keys('name')
+		password_element = self.browser.find_element_by_id("id_email").send_keys('user@name.com')
+
+		password_element = self.browser.find_element_by_id("id_password1").send_keys('1')
+		password_element = self.browser.find_element_by_id("id_password2").send_keys('1')
+
+
+		self.browser.find_element_by_xpath("//button[@type='submit']").click()
+
+		dashboard_url = self.live_server_url + reverse('register')
+		
+		self.assertEquals(
+			self.browser.current_url,
+			dashboard_url
+			)
+
+	def test_registration_with_unmatching_passwords_is_not_permitted(self):
+
+		self.browser.get(self.live_server_url + reverse('register'))
+
+		username_element = self.browser.find_element_by_id("id_username").send_keys('username')
+		first_name_element = self.browser.find_element_by_id('id_first_name').send_keys('user')
+		first_name_element = self.browser.find_element_by_id('id_last_name').send_keys('name')
+		password_element = self.browser.find_element_by_id("id_email").send_keys('user@name.com')
+
+		password_element = self.browser.find_element_by_id("id_password1").send_keys('1')
+		password_element = self.browser.find_element_by_id("id_password2").send_keys('2')
+
+
+		self.browser.find_element_by_xpath("//button[@type='submit']").click()
+
+		dashboard_url = self.live_server_url + reverse('register')
+		
+		self.assertEquals(
+			self.browser.current_url,
+			dashboard_url
+			)
